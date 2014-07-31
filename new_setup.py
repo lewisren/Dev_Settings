e760688ec550744fd8a80ff6d67e92b12a73e4d6
Lewis Ren <lewis.ren@bazaarvoice.com>
2014-07-31 15:56:35 -0500

    First commit with base files

---
+++ new_setup.py
@@ -0,0 +1,10 @@
+# Install Homebrew
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+brew doctor
+
+# Install wget
+brew install wget
+
+# Install pip
+brew install python
+easy_install pip
