fe95a0a79c01e02a6c593611c6175e55cbbd67e2
Lewis Ren <lewisren@gmail.com>
2017-10-26 17:42:48 -0500

    Updated new hidden files and setup script for sierra

--- homebrew_setup.sh
+++ homebrew_setup.sh
@@ -1,10 +1,11 @@
+#!/bin/sh
+
 # Install Homebrew
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-brew doctor
 
 # Install wget
 brew install wget
 
 # Install pip
 brew install python
-easy_install pip
+brew install jq
